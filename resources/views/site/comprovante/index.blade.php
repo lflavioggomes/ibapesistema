f7ceaafac5263aa298ac28029c87dbb3dde58b55
Luiz Flávio <98827318+LuizFlavioGomes@users.noreply.github.com>
2022-08-04 13:18:23 -0300

    Criando Rotas e Controllers do menu Pré-Qualificação

---
+++ resources/views/site/comprovante/index.blade.php
@@ -0,0 +1,17 @@
+@extends('adminlte::page')
+@section('content_header')
+<h1>Comprovante de Pagamento</h1>
+@stop
+
+@section('content')
+teste
+@stop
+@section('css')
+<link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+<script>
+    console.log('Hi!');
+</script>
+@stop
